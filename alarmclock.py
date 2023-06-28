3cb05f5d12397194eec8c3343f94141601d10195
edwinisac <133896653+edwinisac@users.noreply.github.com>
2023-06-28 17:52:36 +0530

    Update alarmclock.py

--- alarmclock.py
+++ alarmclock.py
@@ -8,12 +8,8 @@ def set_alarm(alarm_time):
             print("ALARM!")
             break
         time.sleep(1)
-
-# Get user input for alarm time
 print("Welcome to the Digital Alarm Clock!")
 print("Please enter the alarm time in 24-hour format.")
 print("Example: 08:30:00")
 alarm_time = input("Enter the alarm time: ")
-
-# Start the alarm clock
 set_alarm(alarm_time)
